4973cf2fa42bde3159f79ba7408e455f21d0bd4d
Eminerve <60135711+Eminerve@users.noreply.github.com>
2020-06-18 12:28:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preload" href="style.css" as="style">
+    <link rel="preload" href="script.js" as="script">
+    <link rel="stylesheet" href="style.css">
+
+
+    <title>La Déesse - Home</title>
+  </head>
+  <body>
+
+    <div id="contener">
+        <a href="#navig">
+    		<video src="video/chocolat1.mp4" autoplay poster="images/img choc.jpg" width="100%" height="100%" muted preload="auto" loop></video>
+    		<div class="animate seven">
+    			<span>L</span><span>a</span>&nbsp;<span>D</span><span>é</span><span>e</span><span>s</span><span>s</span><span>e</span>
+    		</div>
+        <div class="scrolling_image">
+          <a href="#navig"><img src="images/fleche.gif" alt="images flèche vers le bas"></a>
+        </div></a>
+    </div>
+
+    <!-- Intégration de la navigation fixe -->
+    <div id="navig">
+      <?php include('navigat.php'); ?>
+    </div>
+
+
+    <div id="bienvenue">
+      <h1>Le culte du Cacao moderne</h1>
+      <p>Depuis plus de trente ans nous avons le plaisir de participer à vos moments de bonheur.<br> De la gourmandise au gâteau de mariage, nous sommes à vos cotés pour satisfaire toutes vos envies.</p>
+    </div>
+
+    <section class="categories">
+      <div class="element">
+        <div class="bouton">
+          <button type="button" name="voir">VOIR</button>
+        </div>
+      </div>
+      <div class="element">
+        <div class="bouton">
+          <button type="button" name="voir">VOIR</button>
+        </div>
+      </div>
+      <div class="element">
+        <div class="bouton">
+          <button type="button" name="voir">VOIR</button>
+        </div>
+      </div>
+      <div class="element">
+        <div class="bouton">
+          <button type="button" name="voir">VOIR</button>
+        </div>
+      </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.0.js"  integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc="  crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("navig");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+    $(document).ready(function() {
+      $('a[href^="#"]').click(function(evt){
+        evt.preventDefault();
+        var target = $(this).attr('href');
+        $('html,body')
+    .stop()
+    .animate({scrollTop: $(target).offset().top}, 1000);
+  });
+
+});
+    </script>
+  </body>
+</html>
---
+++ navigat.php
@@ -0,0 +1,138 @@
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>La Déesse-Navigation</title>
+</head>
+
+<body>
+	<div class="nav">
+
+		<div id="logo">
+			<h1 class="titre1">La Déesse</h1>
+		</div>
+
+		<nav>
+        <ul>
+        		<li><a href="patisserie.php">Pâtisserie</a></li>
+            <li><a href="chocolaterie.php">Chocolatrie</a></li>
+						<li><a href="confiserie.php">Confiserie</a></li>
+            <li><a href="glacerie.php">Glacerie</a></li>
+        		<li><a href="apropos.php">À Propos</a></li>
+            <li><a href="monde.php">Découvertes</a></li>
+      	</ul>
+    </nav>
+
+	</div>
+
+  <style>
+
+  @font-face
+  {
+    font-family: 'Lobster_1.3';
+        src: url('fonts/Lobster_1.3.otf');
+  }
+
+  @font-face {
+        font-family: 'OpenSans';
+        src: url('fonts/OpenSans-Light.ttf');
+        font-style: normal;
+        font-weight: 300;
+        font-display: swap;
+      }
+
+      @font-face {
+        font-family: 'OpenSans';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: url('fonts/OpenSans-Regular.ttf');
+      }
+
+      @font-face {
+        font-family: 'OpenSans';
+        font-style: normal;
+        font-weight: 600;
+        font-display: swap;
+        src: url('fonts/open-sans-semibold.ttf');
+      }
+
+      *{
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+
+        html{
+          font-size: 14px;
+          font-family: 'OpenSans', halvetica, sans-serif,serif;
+        }
+
+  /* -------------------------- Titre ------------------------- */
+
+  .titre1
+  {
+		font-family: 'Lobster_1.3',serif;
+		margin-left: 3rem;
+		color: #581c0c;
+  }
+
+  /* -------------------------- Header -------------------------- */
+  .nav{
+    	display: flex;
+    	flex-direction: row;
+    	width: 100%;
+			height: 3rem;
+			text-align: center;
+			align-items: center;
+			justify-content: center;
+			margin: auto;
+			z-index:1000;
+			background-color: #fae7cb;
+			box-shadow: 0px 1px 8px #581c0c;
+  }
+
+  #logo
+  {
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+  }
+
+  /* ------------------------ Navigation ------------------------ */
+
+  nav
+  {
+    display: flex;
+    justify-content: flex-start;
+    font-weight: 400;
+    font-family: 'OpenSans';
+  }
+  nav ul
+  {
+      list-style-type: none;
+      display: flex;
+  }
+
+  nav li
+  {
+      margin-right: 25px;
+  }
+
+  nav a
+  {
+      font-size: 1rem;
+      color: #581c0c;
+      padding-bottom: 3px;
+      text-decoration: none;
+      text-transform: uppercase;
+  }
+
+  nav a:hover
+  {
+  	  border-bottom: 1px solid #581c0c;
+  }
+  </style>
+
+</body>
+</html>
